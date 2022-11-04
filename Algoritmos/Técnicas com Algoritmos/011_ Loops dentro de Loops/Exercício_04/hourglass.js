bce11de9198106d72627df5c820c5672292d914d
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-11-04 18:50:15 -0300

    Terminado o décimo primeiro módulo do capítulo de Técnicas com Algoritmos da seção de Algoritmos

--- Algoritmos/Técnicas com Algoritmos/011_ Loops dentro de Loops/Exercício_04/hourglass.js
+++ Algoritmos/Técnicas com Algoritmos/011_ Loops dentro de Loops/Exercício_04/hourglass.js
@@ -1,3 +1,60 @@
 function desenhaAmpulheta(n) {
+    for(let i = 0; i < n; i++)  {
+        let str = "";
 
+        for(let j = 0; j < n; j++)  {
+            if(i === 0) {
+                str += "*";
+            }
+
+            else if(i + j ===  n - 1)    {
+                str += "*";
+            }
+
+            else if(i === j)    {
+                str += "*";
+            }
+
+            else if(i === n - 1) {
+                str += "*";
+            }
+
+            else {
+                str += " ";
+            }
+        }
+
+        console.log(str);
+    }
+}
+
+/* Outra solução */
+function desenhaAmpulheta(n){
+    for(let i = 0; i < n ; i++) {
+        let str = "";
+
+        if (i == 0 || i ==  n-1)    {
+
+            for(let j = 0; j < n; j++)  {
+                str += "*";
+            }
+
+        } else  {
+
+            for(let j = 0; j < n; j++)  {
+                if( j == i || j == n - 1 - i)   {
+
+                    str += "*";
+
+                } else  {
+                    
+                    str+= " ";
+                    
+                }
+            }
+        
+        }
+
+        console.log(str);
+    }
 }
